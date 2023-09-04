70e53beb339b4814676b0443c062451301f2fe20
Luis Diego <ldiegohcr@gmail.com>
2023-09-04 16:32:13 -0600

    Avance Pantalla 1

--- start.c
+++ start.c
@@ -10,9 +10,13 @@
 #include <ctype.h>
 
 GtkWidget *window_start;
-GtkWidget *btn_spin;
-GtkWidget *grid1;
-GtkBuilder	*builder;
+GtkWidget *btn_info_estados;
+GtkWidget *info_estados;
+GtkWidget *btn_dialog_1;
+GtkWidget *btn_dialog_2;
+GtkWidget *btn_info_simbolos;
+GtkWidget *info_simbolos;
+GtkBuilder *builder;
 
 int main(int argc, char *argv[]) {
     // Init GTK
@@ -26,8 +30,51 @@ int main(int argc, char *argv[]) {
 
     gtk_builder_connect_signals(builder, NULL);
 
+    btn_info_estados = GTK_WIDGET(gtk_builder_get_object(builder, "btn_info_estados"));
+    btn_info_simbolos = GTK_WIDGET(gtk_builder_get_object(builder, "btn_info_simbolos"));
+
+    info_estados = GTK_WIDGET(gtk_builder_get_object(builder, "info_estados"));
+    btn_dialog_1 = GTK_WIDGET(gtk_builder_get_object(builder, "btn_dialog_1"));
+
+    info_simbolos = GTK_WIDGET(gtk_builder_get_object(builder, "info_simbolos"));
+    btn_dialog_2 = GTK_WIDGET(gtk_builder_get_object(builder, "btn_dialog_2"));
+
     gtk_widget_show(window_start);
     gtk_main();
 
     return EXIT_SUCCESS;
+}
+
+void on_btn_info_estados_clicked(GtkWidget *button) {
+    if (info_estados != NULL) {
+        // Open dialog in the center of the parent window.
+        gtk_window_set_transient_for(GTK_WINDOW(info_estados), GTK_WINDOW(window_start));
+        gtk_window_set_position(GTK_WINDOW(info_estados), GTK_WIN_POS_CENTER_ON_PARENT);
+
+        gtk_dialog_run(GTK_DIALOG(info_estados));
+        gtk_widget_hide(info_estados);
+    }
+}
+
+void on_btn_info_simbolos_clicked(GtkWidget *button) {
+    if(info_simbolos != NULL){
+        // Open dialog in the center of the parent window.
+        gtk_window_set_transient_for(GTK_WINDOW(info_simbolos), GTK_WINDOW(window_start));
+        gtk_window_set_position(GTK_WINDOW(info_simbolos), GTK_WIN_POS_CENTER_ON_PARENT);
+
+        gtk_dialog_run(GTK_DIALOG(info_simbolos));
+        gtk_widget_hide(info_simbolos);
+    }
+}
+
+void on_btn_dialog_1_clicked(GtkWidget *button) {
+    if (info_estados != NULL) {
+        gtk_widget_hide(info_estados);
+    }
+}
+
+void on_btn_dialog_2_clicked(GtkWidget *button) {
+    if(info_simbolos != NULL){
+        gtk_widget_hide(info_simbolos);
+    }
 }
