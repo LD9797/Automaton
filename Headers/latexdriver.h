2d21dd8750b36674761b35c06a274f17a7f347c8
vicky <vicaror201195@gmail.com>
2023-11-01 19:03:50 -0600

    dfa math components

--- Headers/latexdriver.h
+++ Headers/latexdriver.h
@@ -5,5 +5,5 @@
 int install_pdflatex_evince();
 int init_latex_driver(int **table, const int *accept, char **states, const char *symbols, int num_symbols, int num_states);
 void draw_graph();
-
+void dfa_math_components(int *Accept, const char * symbols);
 #endif //AUTOMATON_HEADERS_LATEXDRIVER_H_
