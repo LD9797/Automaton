12fc555c707ba97cef7869c09e6e99ba83a71933
ricalbsa <ricalbsa@gmail.com>
2023-11-04 16:03:31 -0600

    Included table generation and start processing

--- Source/test.c
+++ Source/test.c
@@ -2,219 +2,249 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Expressions {
-    struct TreeNode** expressions;
-    int* states;
+#define STATES_LEN 4
+#define SYMBOLS_LEN 4
+#define STRING_LEN 50
+
+#define INITIALIZE 0
+
+struct Expression {
+    char *value;
     int length;
 };
 
-struct InnerStatesResult {
-    int* states;
+struct Linked_node {
+    int value;
+    struct Linked_node *next;
+};
+
+struct Linked_list {
     int length;
+    struct Linked_node *head;
+    struct Linked_node *tail;
 };
 
-struct TreeNode {
-    char* value;
-    struct TreeNode** children;
-    int numChildren;
-};
+// List methods
 
-struct TreeNode* createNode(char* value) {
-    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-    newNode->value = strdup(value);
-    newNode->children = NULL;
-    newNode->numChildren = 0;
-    return newNode;
+struct Linked_list *initialize_list() {
+    struct Linked_list *list = (struct Linked_list *)malloc(sizeof(struct Linked_list));
+
+    list->length = 0;
+    list->head = NULL;
+    list->tail = NULL;
+
+    return list;
 }
 
-void intToString(int num, char* str, int maxLen) {
-    snprintf(str, maxLen, "%d", num);
-}
+struct Linked_node *initialize_node(int value) {
+    struct Linked_node *node = (struct Linked_node *)malloc(sizeof(struct Linked_node));
 
-int isNumber(const char* str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] < '0' || str[i] > '9') {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void findNumbers(struct TreeNode* node, struct InnerStatesResult* result) {
-    if (node == NULL) {
-        return;
-    }
-
-    if (isNumber(node->value)) {
-        // Si el valor del nodo es un número, agrégalo a la lista de estados
-        result->states[result->length++] = atoi(node->value);
-    }
-
-    for (int i = 0; i < node->numChildren; i++) {
-        findNumbers(node->children[i], result);
-    }
-}
-
-void addChild(struct TreeNode* parent, struct TreeNode* child) {
-    parent->numChildren++;
-    parent->children = (struct TreeNode**)realloc(parent->children, parent->numChildren * sizeof(struct TreeNode*));
-    parent->children[parent->numChildren - 1] = child;
-}
-
-struct TreeNode* factorizeSubtree(struct TreeNode* node) {
-    if (node == NULL || node->numChildren == 0) {
-        return node;
-    }
-
-    for (int i = 0; i < node->numChildren; i++) {
-        node->children[i] = factorizeSubtree(node->children[i]);
-    }
-
-    // Process required
+    node->value = value;
+    node->next = NULL;
 
     return node;
 }
 
-void factorize(struct TreeNode* node) {
-    if (node == NULL) {
-        return;
-    }
+void insert_element(struct Linked_list *list, int value, int position) {
+    struct Linked_node *node = initialize_node(value);
 
-    for (int i = 0; i < node->numChildren; i++) {
-        node->children[i] = factorizeSubtree(node->children[i]);
-    }
-}
+    if (list->tail == NULL) {
+        list->head = node;
+        list->tail = node;
+    } else {
+        if (position < 0 || position >= list->length) {
+            list->tail->next = node;
+            list->tail = node;
+        } else if (position == 0) {
+            node->next = list->head;
+            list->head = node;
+        } else {
+            int current_position = 0;
+            struct Linked_node *current_node = list->head;
 
-void printTree(struct TreeNode* node) {
-    if (node == NULL) {
-        return;
-    }
+            while(current_node != NULL) {
+                if (current_position + 1 == position) {
+                    node->next = current_node->next;
+                    current_node->next = node;
+                    break;
+                }
 
-    printf("%s", node->value);
-
-    if (node->numChildren > 0) {
-        printf("(");
-        for (int i = 0; i < node->numChildren; i++) {
-            printTree(node->children[i]);
-            if (i < node->numChildren - 1) {
-                printf(", ");
+                current_node = current_node->next;
+                current_position++;
             }
         }
-        printf(")");
+    }
+
+    list->length++;
+}
+
+void remove_element(struct Linked_list *list, int value) {
+    struct Linked_node *previous_node = NULL;
+    struct Linked_node *current_node = list->head;
+
+    while(current_node != NULL) {
+        if (current_node->value == value) {
+            if (previous_node == NULL) {
+                list->head = current_node->next;
+            } else {
+                previous_node->next = current_node->next;
+            }
+
+            free(current_node);
+            return;
+        }
+
+        previous_node = current_node;
+        current_node = current_node->next;
     }
 }
 
-struct InnerStatesResult InnerStates(struct TreeNode* node) {
-    struct InnerStatesResult result;
-    result.states = (int*)malloc(100 * sizeof(int));
-    result.length = 0;
+int contains_element(struct Linked_list *list, int value) {
+    struct Linked_node *current_node = list->head;
 
-    findNumbers(node, &result);
+    while(current_node != NULL) {
+        if (current_node->value == value) {
+            return 1;
+        }
 
-    return result;
+        current_node = current_node->next;
+    }
+
+    return 0;
 }
 
-struct Expressions CalcEquations(int* estados, int tamEstados, int* aceptacion, int tamAceptacion, char** simbolos, int tamSimbolos, int** transiciones) {
-    int* estadosExpresiones = (int*)malloc(tamEstados * sizeof(int));
-    struct TreeNode** expresionesPorEstado = (struct TreeNode**)malloc(tamEstados * sizeof(struct TreeNode*));
-    int* procRequeridos = (int*)malloc(tamEstados * sizeof(int));
-    struct TreeNode** totalExpresiones = (struct TreeNode**)malloc(tamEstados * sizeof(struct TreeNode*));
+// Expression methods
 
-    // Inicializa estadosExpresiones y procRequeridos según los estados de aceptación
-    int contExpresionesPorEstado = 0;
-    int contProcRequeridos = tamAceptacion;
+void print_table(struct Expression **expression_table, int rows, int columns) {
 
-    for (int i = 0; i < tamEstados; i++) {
-        estadosExpresiones[i] = 0;
-        procRequeridos[i] = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%s\t", (expression_table[i][j].value[0] != '\0') ? expression_table[i][j].value : "-");
+        }
+        printf("\n");
     }
+}
 
-    for (int i = 0; i < tamAceptacion; i++) {
-        estadosExpresiones[aceptacion[i] - 1] = 1; // Marcamos los estados de aceptación
-        expresionesPorEstado[i] = createNode(""); // Inicializamos los árboles en estados de aceptación
+void modify_expression(struct Expression *expression, const char *value, int length, int rule) {
+    if (rule == INITIALIZE) {
+        if (expression->length == 0) {
+            strcpy(expression->value, value);
+            expression->length = (int)strlen(expression->value);
+        } else {
+            char temp[STRING_LEN];
+            char *open_paren = strchr(expression->value, '(');
+            char *close_paren = strrchr(expression->value, ')');
+
+            if (open_paren != NULL && close_paren != NULL && open_paren < close_paren) {
+                *close_paren = '\0';
+                snprintf(temp, sizeof(temp), "%s+%s", expression->value + 1, value);
+            } else {
+                snprintf(temp, sizeof(temp), "(%s+%s)", expression->value, value);
+            }
+
+            strcpy(expression->value, temp);
+            expression->length = (int)strlen(expression->value);
+        }
+    } else {
+
     }
+}
 
-    // Inicializa totalExpresiones con nodos vacíos
-    for (int i = 0; i < tamEstados; i++) {
-        totalExpresiones[i] = createNode("");
-    }
+struct Expression ** create_expression_table(int **transitions, char **symbols, int states_len, int symbols_len, int verbose) {
+    int rows = states_len;
+    int columns = states_len + 1;
 
-    for (int i = 0; i < tamEstados; i++) {
-        for (int j = 0; j < tamSimbolos; j++) {
-            int posArbol = transiciones[i][j] - 1;
-
-            char representNum[10];
-            intToString(estados[i], representNum, sizeof(representNum));
-            addChild(totalExpresiones[posArbol], createNode(representNum));
-            addChild(totalExpresiones[posArbol], createNode("*"));
-            addChild(totalExpresiones[posArbol], createNode(simbolos[j]));
+    struct Expression **expression_table = (struct Expression **)malloc(rows * sizeof(struct Expression *));
+    for (int i = 0; i < rows; i++) {
+        expression_table[i] = (struct Expression *)malloc(columns * sizeof(struct Expression));
+        for (int j = 0; j < columns; j++) {
+            expression_table[i][j].length = 0;
+            expression_table[i][j].value = (char *)malloc(STRING_LEN * sizeof(char));
+            expression_table[i][j].value[0] = '\0';
         }
     }
 
-    // Procesa estados requeridos
-    while (contProcRequeridos != 0) {
-        int* tempRequeridos = (int*)malloc(tamEstados * sizeof(int));
-        int tempCount = 0;
-
-        for (int i = 0; i < tamEstados; i++) {
-            tempRequeridos[i] = procRequeridos[i];
+    modify_expression(&expression_table[0][columns - 1], "ε", 1, INITIALIZE);
+    for (int i = 0; i < states_len; i++) {
+        for (int j = 0; j < symbols_len; j++) {
+            modify_expression(&expression_table[transitions[i][j] - 1][i], symbols[j], 1, INITIALIZE);
         }
+    }
 
-        contProcRequeridos = 0;
+    if (verbose == 1) {
+        print_table(expression_table, rows, columns);
+    }
 
-        for (int i = 0; i < tamEstados; i++) {
-            if (tempRequeridos[i] && !estadosExpresiones[i]) {
-                estadosExpresiones[i] = 1;
-                expresionesPorEstado[contExpresionesPorEstado] = totalExpresiones[i];
-                contExpresionesPorEstado++;
+    return expression_table;
+}
 
-                struct InnerStatesResult estadosEncontrados = InnerStates(totalExpresiones[i]);
+void process_expression_table(struct Expression **expression_table, const int *accept, char **symbols, int states_len, int symbols_len, int accept_len, int verbose) {
+    struct Linked_list *pending = initialize_list();
 
-                for (int j = 0; j < estadosEncontrados.length; j++) {
-                    if (!procRequeridos[estadosEncontrados.states[j] - 1]) {
-                        procRequeridos[estadosEncontrados.states[j] - 1] = 1;
-                        contProcRequeridos++;
+    for (int i = 0; i < accept_len; i++) {
+        insert_element(pending, accept[i], -1);
+    }
+
+    do {
+        for (int i = 0; i < pending->length; i++) {
+            int expression_checked = accept[i];
+
+            for (int j = 0; j < states_len; j++) {
+                struct Expression *current_expression = &expression_table[expression_checked][j];
+                if (expression_checked != j && current_expression->length != 0) {
+                    if (expression_table[j][j].length == 0) {
+                        // TODO: resolve_expression(&expression_table, expression_checked, j);
+                    } else {
+                        struct Linked_list *excluded = initialize_list();
+                        insert_element(excluded, expression_checked, -1);
+                        // TODO: int resolve_expression_dependency(&expression_table, j, &excluded);
+                        // TODO: if 1 try resolve_expression(&expression_table, expression_checked, j);
+                        // TODO: free excluded_list memory
                     }
                 }
             }
+
+            // TODO: check if expression is ready for final arden
         }
 
-        free(tempRequeridos);
+        // TODO: remove from pending those states that have received a final arden
+    } while (pending->length != 0);
+
+    if (verbose == 1) {
+        print_table(expression_table, states_len, states_len + 1);
     }
-
-    free(procRequeridos);
-
-    // Realiza un bucle para imprimir las expresiones
-    for (int i = 0; i < tamEstados; i++) {
-        if (estadosExpresiones[i]) {
-            printf("Expresión para Estado %d: ", estados[i]);
-            printTree(expresionesPorEstado[i]);
-            printf("\n");
-        }
-    }
-
-    struct Expressions result;
-    result.expressions = expresionesPorEstado;
-    result.states = estadosExpresiones;
-    result.length = contExpresionesPorEstado;
-
-    return result;
 }
 
 int main() {
-    int estados[] = {1, 2, 3, 4};
-    int aceptacion[] = {1, 2, 3};
-    char* simbolos[] = {"A", "T", "C", "G"};
-    int transiciones[4][4] = {{4, 2, 3, 1}, {1, 2, 3, 1}, {4, 2, 4, 1}, {4, 4, 4, 4}};
+    int accept_states[] = {1, 1, 1, 0};
+    char *symbols[] = {"A", "T", "C", "G"};
+    int table[STATES_LEN][STATES_LEN] = {
+            {4, 2, 3, 1},
+            {1, 2, 3, 1},
+            {4, 2, 4, 1},
+            {4, 4, 4, 4}
+    };
 
-    struct Expressions result = CalcEquations(estados, 4, aceptacion, 3, simbolos, 4, transiciones);
-
-    for (int i = 0; i < result.length; i++) {
-        free(result.expressions[i]);
+    // Adapt table to readable format
+    int *transitions[STATES_LEN];
+    for (int i = 0; i < STATES_LEN; i++) {
+        transitions[i] = table[i];
     }
 
-    free(result.expressions);
-    free(result.states);
+    // Convert one-hot-vector and states
+    int accept_len = 0;
+    int *states = (int *)malloc(STATES_LEN * sizeof(int));
+    int *accept = (int *)malloc(STATES_LEN * sizeof(int));
+    for (int i = 0; i < STATES_LEN; i++) {
+        states[i] = i;
+        if (accept_states[i] == 1) {
+            accept[accept_len] = accept_len;
+            accept_len++;
+        }
+    }
 
+    struct Expression **expression_table = create_expression_table(transitions, symbols, STATES_LEN, SYMBOLS_LEN, 1);
+    process_expression_table(expression_table, accept, symbols, STATES_LEN, SYMBOLS_LEN, accept_len, 1);
+    // TODO: free { expression_table, states, accept } memory
     return 0;
 }
